205798496e90cdd345ce165128b4473b1bbced04
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-08-07 23:22:55 +0530

    added avatar in header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,20 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-const Header = () => {
-  const path = useLocation.pathname;
+export default function Header() {
+  const path = useLocation().pathname;
+
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <Navbar className="border-b-2 ">
+    <Navbar className="border-b-2">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white "
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Manish's
@@ -19,20 +24,44 @@ const Header = () => {
       <form>
         <TextInput
           type="text"
-          placeholder="search"
-          className="hidden lg:inline"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+        <Button>
           <FaMoon />
         </Button>
-        <Link to="/signin">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
@@ -48,6 +77,4 @@ const Header = () => {
       </Navbar.Collapse>
     </Navbar>
   );
-};
-
-export default Header;
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-
+import OAuth from '../components/OAuth'
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
