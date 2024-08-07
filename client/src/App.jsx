cc3d0d042eb567839bc41247ed30c4ef07c9eda7
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-08-07 15:01:27 +0530

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn/'
 import SignUp from './pages/SignUp.jsx'
 import Dashboard from './pages/Dashboard/'
 import Header from './components/Header/'
+import Footer from './components/Footer/'
 
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
       <Route path='/signin' element={<SignIn />}/>
       <Route path='/signup' element={<SignUp />}/>
     </Routes>
+    <Footer />
     </BrowserRouter>
   )
 }
