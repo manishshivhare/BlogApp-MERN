9061daa0aeab570f8cd3919e3aa866d92ede5dbe
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-08-07 15:22:00 +0530

    added signin api and logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken'
+import { errorHandler } from '../utils/error.js';
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -24,10 +26,44 @@ const signup = async (req, res) => {
   try {
     await newUser.save();
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 
   res.json({ message: "signup successfull" });
 };
 
-export { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET, {expiresIn: '7d'}
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin };
