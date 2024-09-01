e7e9509cd5bfc19d60a41adfb242e53fb0907964
Manish Shivhare <67515371+manishshivhare@users.noreply.github.com>
2024-09-02 01:53:33 +0530

    bug fix

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,19 +5,19 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+               Play 2048 game
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+            2048 Puzzle Game is an addictive and challenging
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://manish-shivhare-2048.netlify.app/" target='_blank' rel='noopener noreferrer'>
+                    Play now!
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJ0kjnyMMmDvtS6oiZYbOo6pdiRh2h52EfA&s" />
         </div>
     </div>
   )
